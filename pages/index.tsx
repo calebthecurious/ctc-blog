63ba6fa365452575841ba580efb4f695dc8879f0
calebthecurious <calebthecurious@gmail.com>
2023-02-20 19:44:16 +1100

    4th commit - right widget component

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,11 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>ctc Blog</title>
+        <title>The Curious Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-4 col-span-1">
+        <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
             <PostCard post={post.node} key={post.title} />
           ))}
