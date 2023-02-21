2c6681048402d366287f0b5dd625fbac6b6ea5cb
calebthecurious <calebthecurious@gmail.com>
2023-02-21 13:18:02 +1100

    6th commit - Added Article Details

--- services/index.js
+++ services/index.js
@@ -40,6 +40,43 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          ... on Author {
+            id
+            name
+            bio
+            photo {
+              url
+            }
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
     query GetPostDetails() {
@@ -79,7 +116,7 @@ export const getSimilarPosts = async (categories, slug) => {
     }
   `;
 
-  const result = await request(graphqlAPI, query, { slug, categories });
+  const result = await request(graphqlAPI, query, { categories, slug });
 
   return result.posts;
 };
