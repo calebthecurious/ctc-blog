b3a4aa715d3f210475b1a4fdbf43b743c763769d
calebthecurious <calebthecurious@gmail.com>
2023-02-22 23:46:12 +1100

    7th commit - added comments 1/2

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>
-      <h1>Author</h1>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      .
+      <Image
+        // src={author.photo.url}
+        // alt={author.name}
+        unoptimized
+        height="100px"
+        width="100px"
+        className="align-middle rounded-full"
+      />
+      {/* <h3 className="text-white my-4 text-xl font-bold">AUTHOR</h3>
+      <p className="text-white text-lg">{author.bio}</p> */}
     </div>
   );
 };
